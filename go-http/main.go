8fbae954edf4d47ed6cda2fb35c19dfc501afc6f
Luke M <44519853+lkdm@users.noreply.github.com>
2025-07-09 10:23:23 +1000

    chore(go): create endpoint handlers

--- go-http/main.go
+++ go-http/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+	"fmt"
+)
+
+func healthCheckHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "ok\n")
+}
+
+
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func setupHandlers(mux *http.ServeMux) {
+	mux.HandleFunc("/health", healthCheckHandler)
+	mux.HandleFunc("/api", apiHandler)
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	// Set up listening address
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	if len(listenAddr) == 0 {
+		listenAddr = ":8080"
+	}
+
+	// Set up handlers
+	mux := http.NewServeMux()
+	setupHandlers(mux)
+
+	log.Fatal(http.ListenAndServe(listenAddr, mux))
 }
+
