0b5193996bd527cc7e44e21adc97c0dacd66fbd4
Luke M <44519853+lkdm@users.noreply.github.com>
2025-07-17 09:30:40 +1000

    chore(rust-axum): add patch, delete verbs

--- rust-axum/src/users.rs
+++ rust-axum/src/users.rs
@@ -3,11 +3,12 @@ use super::{AppState, Db};
 use crate::types::Identifier;
 use axum::{
     extract::{FromRef, Path, State},
-    response::Json,
+    response::{IntoResponse, Json},
 };
 use axum_extra::routing::Resource;
 use chrono::NaiveDateTime;
-use serde::Serialize;
+use http::StatusCode;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, sqlx::FromRow, Serialize)]
 pub struct User {
@@ -15,6 +16,11 @@ pub struct User {
     created_date: NaiveDateTime,
     modified_date: NaiveDateTime,
     deleted_date: Option<NaiveDateTime>,
+}
+
+#[derive(Deserialize)]
+pub struct UpdateUser {
+    pub deleted_date: Option<NaiveDateTime>,
 }
 
 #[derive(Clone)]
@@ -77,6 +83,28 @@ impl Users {
         .fetch_one(&self.db)
         .await
     }
+
+    pub async fn update(&self, id: Identifier, payload: UpdateUser) -> sqlx::Result<User> {
+        sqlx::query_as::<_, User>(
+            r#"
+                UPDATE users
+                SET deleted_date = ?
+                WHERE id = ?
+                RETURNING id, created_date, modified_date, deleted_date
+            "#,
+        )
+        .bind(payload.deleted_date)
+        .bind(id)
+        .fetch_one(&self.db)
+        .await
+    }
+
+    pub async fn delete(&self, id: Identifier) -> sqlx::Result<()> {
+        sqlx::query!(r#"DELETE FROM users WHERE id = ?"#, id)
+            .execute(&self.db)
+            .await?;
+        Ok(())
+    }
 }
 
 async fn index(queries: State<Users>) -> crate::Result<Json<Vec<User>>> {
@@ -94,10 +122,29 @@ async fn create(queries: State<Users>) -> crate::Result<Json<User>> {
     Ok(Json(user))
 }
 
+async fn edit(
+    State(queries): State<Users>,
+    Path(id): Path<Identifier>,
+    Json(payload): Json<UpdateUser>,
+) -> crate::Result<Json<User>> {
+    let user = queries.update(id, payload).await?;
+    Ok(Json(user))
+}
+
+async fn delete(
+    State(queries): State<Users>,
+    Path(id): Path<Identifier>,
+) -> crate::Result<impl IntoResponse> {
+    queries.delete(id).await?;
+    Ok(StatusCode::NO_CONTENT)
+}
+
 pub fn router() -> Resource<AppState> {
     Resource::named("users")
         .index(index) // GET /users
         .create(create) // POST /users
         .show(show) // GET /users/:id
+        .update(edit) // PATCH /users/:id
+        .destroy(delete) // DELETE /users/:id
         .into()
 }
