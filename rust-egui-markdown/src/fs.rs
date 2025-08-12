8ba716f7612cfe66a0574820312d912c820dd792
Luke M <44519853+lkdm@users.noreply.github.com>
2025-08-13 09:17:40 +1000

    chore: fs improvements

--- rust-egui-markdown/src/fs.rs
+++ rust-egui-markdown/src/fs.rs
@@ -17,80 +17,48 @@ pub enum PersistenceError {
     #[error("I/O error: {0}")]
     IO(#[from] std::io::Error),
 
-    #[error("Registry error: {0}")]
-    Registry(#[from] DataRegistryError),
-
     #[error("Invalid file format")]
     InvalidFormat,
-}
 
-#[derive(Debug, Error)]
-pub enum DataRegistryError {
-    #[error("File not found")]
+    #[error("Entry not found in registry")]
     NotFound,
 }
 
-/// Maps unique Ids to storage handles
-/// For example, a PathBuf or Url
-struct DataRegistry<K, V>(BTreeMap<K, V>);
+/// PersistenceHandler
+///
+/// An abstracted interface over any method of persistence.
+pub trait PersistenceHandler<T> {
+    type Id: Into<Uuid>;
 
-impl<K, V> DataRegistry<K, V>
-where
-    K: Into<Uuid> + Ord,
-{
-    pub fn new() -> Self {
-        Self(BTreeMap::new())
-    }
+    /// Loads and deserializes data associated with this ID.
+    ///
+    /// On FS: opens a file dialog to select and load a file.
+    fn get(&self, id: Self::Id) -> Result<T, PersistenceError>;
 
-    fn get(&self, id: &K) -> Result<&V, DataRegistryError> {
-        // Use `self.0` to access the internal BTreeMap of the tuple struct.
-        self.0.get(id).ok_or(DataRegistryError::NotFound)
-    }
+    /// Creates and stores a new instance of `T`.
+    ///
+    /// On FS: opens a file dialog prompting user to save location,
+    /// writes data to a new file, and tracks it internally.
+    fn new(&mut self, id: Self::Id, value: T) -> Result<T, PersistenceError>;
 
-    fn insert(&mut self, id: K, value: V) {
-        self.0.insert(id, value);
-    }
+    /// Updates an existing persisted object.
+    ///
+    /// On FS: overwrites the file with updated contents.
+    fn update(&mut self, id: Self::Id, value: T) -> Result<T, PersistenceError>;
 
-    fn remove(&mut self, id: &K) {
-        self.0.remove(id);
-    }
+    /// Deletes persisted object.
+    fn remove(&mut self, id: Self::Id) -> Result<(), PersistenceError>;
 }
 
-struct FileSystem<K> {
-    /// A mapping of entity IDs to PathBufs so that we can abstract away the file system
-    registry: DataRegistry<K, PathBuf>,
+struct FileSystem {
+    paths: BTreeMap<Uuid, PathBuf>,
     default_dir: Option<PathBuf>,
+    filter_name: String,
+    extensions: Vec<String>,
 }
 
-impl<K> FileSystem<K>
-where
-    K: Into<Uuid> + Ord,
-{
-    pub fn new(default_dir: Option<PathBuf>) -> Self {
-        Self {
-            registry: DataRegistry::<K, PathBuf>::new(),
-            default_dir,
-        }
-    }
-
-    pub fn registry(&self) -> &DataRegistry<K, PathBuf> {
-        &self.registry
-    }
-
-    /// `reader` - returns a `BufReader` for reading file
-    fn reader(&self, path: PathBuf) -> Result<BufReader<File>, PersistenceError> {
-        let file = File::open(path)?;
-        Ok(BufReader::new(file))
-    }
-
-    /// `writer` - returns a `BufWriter` for writing to a file
-    fn writer(&mut self, path: PathBuf) -> Result<BufWriter<File>, PersistenceError> {
-        let file = File::create(path)?;
-        let mut writer = BufWriter::new(file);
-        Ok(writer)
-    }
-
-    fn rfd(&self) -> rfd::FileDialog {
+impl FileSystem {
+    fn new_dialogue(&self) -> rfd::FileDialog {
         let default_directory = self
             .default_dir
             .clone()
@@ -101,40 +69,92 @@ where
                     .unwrap_or_else(|| PathBuf::from("."))
             });
 
+        let extensions: Vec<&str> = self.extensions.iter().map(|s| s.as_str()).collect();
+
         rfd::FileDialog::new()
-            .add_filter("Markdown files", &[".md"])
+            .add_filter(&self.filter_name, &extensions)
             .set_directory(default_directory)
             .set_can_create_directories(true)
     }
 }
 
-impl DocumentRepository for FileSystem<DocumentId> {
-    fn save(&mut self, document: &Document) -> Result<(), PersistenceError> {
-        let id = document.id();
-
-        // Get the file path from the file dialog; if None, return Ok(()) (no-op)
-        let path = match self.rfd().save_file() {
-            Some(p) => p,
-            None => return Ok(()), // No file selected, exit gracefully
-        };
-
-        // Write the document content to the selected file
-        let mut writer = self.writer(path.clone())?;
-        document.text().write_to(writer)?;
-
-        // Register the file path in the registry after successful write
-        self.registry().insert(id.clone(), path);
-
-        Ok(())
-    }
-
-    fn load(&self, id: DocumentId) -> Result<Document, PersistenceError> {
-        self.read(id.0).map_err(|e| e.to_string()) // Assuming DocumentId can be dereferenced to Uuid
-
-        // let path = self.registry.get(&id)?;
-    }
-
-    fn list(&self) -> Result<Vec<Document>, PersistenceError> {
-        todo!()
-    }
+impl PersistenceHandler<Document> for FileSystem {
+    type Id = Uuid;
 }
+
+// impl FileSystem
+// where
+//     K: Into<Uuid> + Ord,
+// {
+//     pub fn new(default_dir: Option<PathBuf>) -> Self {
+//         Self {
+//             registry: DataRegistry::<K, PathBuf>::new(),
+//             default_dir,
+//         }
+//     }
+//
+//     pub fn registry(&self) -> &DataRegistry<K, PathBuf> {
+//         &self.registry
+//     }
+//
+//     /// `reader` - returns a `BufReader` for reading file
+//     fn reader(&self, path: PathBuf) -> Result<BufReader<File>, PersistenceError> {
+//         let file = File::open(path)?;
+//         Ok(BufReader::new(file))
+//     }
+//
+//     /// `writer` - returns a `BufWriter` for writing to a file
+//     fn writer(&mut self, path: PathBuf) -> Result<BufWriter<File>, PersistenceError> {
+//         let file = File::create(path)?;
+//         let mut writer = BufWriter::new(file);
+//         Ok(writer)
+//     }
+//
+//     fn rfd(&self) -> rfd::FileDialog {
+//         let default_directory = self
+//             .default_dir
+//             .clone()
+//             // If not, the downloads directory
+//             .unwrap_or_else(|| {
+//                 download_dir()
+//                     // Failing all else, just use the current directory
+//                     .unwrap_or_else(|| PathBuf::from("."))
+//             });
+//
+//         rfd::FileDialog::new()
+//             .add_filter("Markdown files", &[".md"])
+//             .set_directory(default_directory)
+//             .set_can_create_directories(true)
+//     }
+// }
+//
+// impl DocumentRepository for FileSystem<DocumentId> {
+//     fn save(&mut self, document: &Document) -> Result<(), PersistenceError> {
+//         let id = document.id();
+//
+//         // Get the file path from the file dialog; if None, return Ok(()) (no-op)
+//         let path = match self.rfd().save_file() {
+//             Some(p) => p,
+//             None => return Ok(()), // No file selected, exit gracefully
+//         };
+//
+//         // Write the document content to the selected file
+//         let mut writer = self.writer(path.clone())?;
+//         document.text().write_to(writer)?;
+//
+//         // Register the file path in the registry after successful write
+//         self.registry().insert(id.clone(), path);
+//
+//         Ok(())
+//     }
+//
+//     fn load(&self, id: DocumentId) -> Result<Document, PersistenceError> {
+//         self.read(id.0).map_err(|e| e.to_string()) // Assuming DocumentId can be dereferenced to Uuid
+//
+//         // let path = self.registry.get(&id)?;
+//     }
+//
+//     fn list(&self) -> Result<Vec<Document>, PersistenceError> {
+//         todo!()
+//     }
+// }
