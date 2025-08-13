4981debda69699d8b8d87becc130259a42734110
Luke M <44519853+lkdm@users.noreply.github.com>
2025-08-14 09:45:02 +1000

    chore: update tests to nest describes

--- bun-xml/morph.test.ts
+++ bun-xml/morph.test.ts
@@ -1,32 +1,37 @@
 import { describe, expect, expectTypeOf, test } from "bun:test";
 import { flattenCollectionDicts } from "./morph";
 
-describe("flattenCollectionDicts", () => {
-	const fn = flattenCollectionDicts;
-	const data = {
-		"2": {
-			test: "B",
-		},
-		"3": {
-			test: "C",
-		},
-		"1": {
-			test: "A",
-		},
-	};
-	test("ok", () => {
-		expectTypeOf(fn(data)).toBeArray;
-	});
-	test("non-contiguous number key", () => {
-		expectTypeOf(fn({ data, "5": true })).toMatchTypeOf<
-			Record<string, unknown>
-		>();
-		expectTypeOf(fn({ data, "5": true })).not.toBeArray();
-	});
-	test("non-numeric key", () => {
-		expectTypeOf(fn({ data, a: true })).toMatchTypeOf<
-			Record<string, unknown>
-		>();
-		expectTypeOf(fn({ data, a: true })).not.toBeArray();
-	});
+describe("XML->JSON Parser", () => {
+  describe("flattenCollectionDicts", () => {
+    const fn = flattenCollectionDicts;
+    const data = {
+      "2": {
+        test: "B",
+      },
+      "3": {
+        test: "C",
+      },
+      "1": {
+        test: "A",
+      },
+    };
+    test("Should flatten", () => {
+      // If the input dictionary's keys are numeric strings, contiguous, and start from 0 or 1; then it should be collapsed into an array.
+      expectTypeOf(fn(data)).toBeArray;
+    });
+    test("Noop because non-contiguous number key", () => {
+      // If the input dictionary's keys are non-contiguous, then it should retain its original shape
+      expectTypeOf(fn({ data, "5": true })).toMatchTypeOf<
+        Record<string, unknown>
+      >();
+      expectTypeOf(fn({ data, "5": true })).not.toBeArray();
+    });
+    test("Noop because non-numeric key", () => {
+      // If any of the input dictionary's keys are non-numeric, it should retain its original shape
+      expectTypeOf(fn({ data, a: true })).toMatchTypeOf<
+        Record<string, unknown>
+      >();
+      expectTypeOf(fn({ data, a: true })).not.toBeArray();
+    });
+  });
 });
