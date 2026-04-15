deec78613a45098dd82700efa61066b9da9fdda1
Luke Martin <luke.martin@26dgm.com>
2026-04-16 09:18:08 +1000

    chore: update

--- py-sqlalchemy/src/data/user.py
+++ py-sqlalchemy/src/data/user.py
@@ -1,13 +1,9 @@
-from sqlalchemy import Integer, String, ForeignKey
-from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
-from typing import Optional
-
-# declarative base class
-class Base(DeclarativeBase):
-    pass
-
+from typing import Optional, List
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import Session
+from data import Base
 
 class User(Base):
     __tablename__ = "user"
@@ -16,3 +12,6 @@ class User(Base):
     name: Mapped[str]
     fullname: Mapped[str] = mapped_column(String(30))
     nickname: Mapped[Optional[str]]
+
+def get_users(db: Session) -> List[User]:
+    return db.query(User).all()
--- py-sqlalchemy/src/main.py
+++ py-sqlalchemy/src/main.py
@@ -1,15 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from models import Base, User
-from typing import List
+from data import Base
+from data.user import User, get_users
 
 # an Engine, which the Session will use for connection
 # resources
 # engine = create_engine("sqlite:///file:database.db?"
 #     "check_same_thread=true&timeout=10&nolock=1&uri=true")
 
-def get_users(db: Session) -> List[User]:
-    return db.query(User).all()
 
 def main():
     engine = create_engine("sqlite:///:memory:")
